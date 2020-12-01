0a99902913a4d0871dd8690aeb95a2e3ea1dfcc5
biubiubiuxixi <843424294@qq.com>
2020-12-01 19:35:58 +0800

    feat:setstate

--- main.js
+++ main.js
@@ -3,15 +3,25 @@
  * @Author: chelsea.jiang
  * @Date: 2020-11-30 18:59:12
  * @LastEditors: chelsea.jiang
- * @LastEditTime: 2020-11-30 21:15:46
+ * @LastEditTime: 2020-12-01 19:24:15
  */
 import xixiToy, { Component } from './xixiToy.js';
 const { render } = xixiToy;
 class MyComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            a: 1,
+            b: 2,
+        };
+    }
     render() {
         return (
             <div>
-                <h1>111111</h1>
+                <h1>MyComponent</h1>
+                <button onClick={() => { this.setState({ a: this.state.a + 1 }) }}>加一</button>
+                <div>{this.state.a.toString()}</div>
+                <div>{this.state.b.toString()}</div>
                 {this.children}
             </div>
         );
--- xixiToy.js
+++ xixiToy.js
@@ -3,8 +3,9 @@
  * @Author: chelsea.jiang
  * @Date: 2020-11-30 20:20:10
  * @LastEditors: chelsea.jiang
- * @LastEditTime: 2020-11-30 21:25:30
+ * @LastEditTime: 2020-12-01 19:28:58
  */
+const RENDER_TO_DOM = Symbol("render to dom"); // 私有属性
 
 const xixiToy = {
     createElement,
@@ -16,16 +17,32 @@ class ElementWrapper {
         this.root = document.createElement(type);
     }
     setAttribute(name, value) {
-        this.root.setAttribute(name, value);
+        if (name.match(/^on([\s\S]+)$/)) {
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value);
+        } else {
+            this.root.setAttribute(name, value);
+        }
     }
     appendChild(component) {
-        this.root.appendChild(component.root);
+        let range = document.createRange();
+        // 因为是appendChild 所以是插在最后
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        component[RENDER_TO_DOM](range);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents(); // 删除内容
+        range.insertNode(this.root); 
     }
 }
 
 class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content);
+    }
+    [RENDER_TO_DOM](range) {
+        range.deleteContents(); // 删除内容
+        range.insertNode(this.root); 
     }
 }
 
@@ -34,6 +51,7 @@ export class Component {
         this._root = null;
         this.props = Object.create(null);
         this.children = [];
+        this._range = null;
     }
     setAttribute(name, value) {
         this.props[name] = value;
@@ -41,11 +59,36 @@ export class Component {
     appendChild(component) {
         this.children.push(component);
     }
-    get root() {
-        if (!this._root) {
-            this._root = this.render().root;
+    /**
+     * 一个递归过程
+     * @param {*} range 重新渲染
+     * @return {*}
+     */
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        this.render()[RENDER_TO_DOM](range);
+    }
+    reRender() {
+        this._range.deleteContents();
+        this[RENDER_TO_DOM](this._range);
+    }
+    setState(newState) {
+        if (this.state === null || typeof this.state !== 'object') {
+            this.state = newState;
+            this.reRender();
+            return;
         }
-        return this._root;
+        let merge = (oldState, newState) => {
+            for (let p in newState) {
+                if (oldState[p] === null || typeof oldState[p] !== "object") {
+                    oldState[p] = newState[p];
+                } else {
+                    merge(oldState[p], newState[p]); // 深拷贝
+                }
+            }
+        }
+        merge(this.state, newState);
+        this.reRender();
     }
 }
 
@@ -85,7 +128,11 @@ function createElement(type, attributes, ...children) {
 }
 
 function render(component, parentElement) {
-    parentElement.appendChild(component.root);
+    let range = document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
 
 export default xixiToy;
